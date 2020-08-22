210ffbb2ecdb35f5d13a20273a1ac12a15e52230
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-08-23 02:18:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
 
 function App() {
+  const element = useRef(null);
+  useEffect(() => {
+    element.current.focus();
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={element} type="text">
+
+      </input>
     </div>
   );
 }
