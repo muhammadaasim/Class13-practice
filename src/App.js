2605af67615960a4eadc8bf8af94345002c63482
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-08-26 22:04:11 +0500

    added animation effect bounce

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React from "react";
 import './App.css'
-import useWebAnimations from "@wellyshen/use-web-animations";
+import useWebAnimations, { bounce, fadeIn, backOutUp, bounceInLeft } from "@wellyshen/use-web-animations";
 
 function App() {
   // const element = useRef(null);
-  const { ref, playState, getAnimation } = useWebAnimations({
-    keyframes: [
-      { transform: "translate(0,0)" },
-      { transform: "translate(800px,0)" }
-    ],
-    timing: {
-      // delay: 400, // Start with a 500ms delay
-      duration: 2500, // Run for 1000ms
-      iterations: Infinity, // Repeat once
-      direction: "alternate", // Run the animation forwards and then backwards
-      easing: "ease-in-out", // Use a fancy timing function
-    },
-    // onReady: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
-    // },
-    // onUpdate: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation enters the running state or changes state
-    // },
-    // onFinish: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-    // },
-    // More useful options...
-  });
+  const { ref, playState, getAnimation } = useWebAnimations({ ...bounceInLeft });
+  // const { ref, playState, getAnimation } = useWebAnimations({
+  //   keyframes: [
+  //     { transform: "translate(0,0)" },
+  //     { transform: "translate(800px,0)" }
+  //   ],
+  //   timing: {
+  //     // delay: 400, // Start with a 500ms delay
+  //     duration: 2500, // Run for 1000ms
+  //     iterations: Infinity, // Repeat once
+  //     direction: "alternate", // Run the animation forwards and then backwards
+  //     easing: "ease-in-out", // Use a fancy timing function
+  //   },
+  // onReady: ({ playState, animate, animation }) => {
+  //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+  // },
+  // onUpdate: ({ playState, animate, animation }) => {
+  //   // Triggered when the animation enters the running state or changes state
+  // },
+  // onFinish: ({ playState, animate, animation }) => {
+  //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+  // },
+  // More useful options...
+  // });
 
   return (
     <div>
