53d44ff92fc09f8db8da33837a242c7025a798ad
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-08-26 17:57:44 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { useRef, useEffect } from 'react';
-import './App.css'
-function App() {
+import React, { useRef, useEffect } from "react";
+import useWebAnimations from "@wellyshen/use-web-animations";
+
+const App = () => {
   const element = useRef(null);
-  useEffect(() => {
-    // element.current.focus();
-  }, []);
+  const { ref, playState } = useWebAnimations({
+    keyframes: {
+      transform: ["translateX(500px)"], // Move by 500px
+      background: ["red", "blue", "green"], // Go through three colors
+    },
+    timing: {
+      delay: 500, // Start with a 500ms delay
+      duration: 1000, // Run for 1000ms
+      iterations: 2, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+    // onReady: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+    // },
+    // onUpdate: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the running state or changes state
+    // },
+    // onFinish: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    // },
+    // More useful options...
+  });
+
   return (
     <div>
-      {/* <input ref={element} type="text"/> */}
-      <div className="target" ref={element}>
+      <div className="target" ref={ref}>
         Hello World!
       </div>
     </div>
   );
-}
-
+};
 export default App;
