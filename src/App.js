64b09f4036822f323504db6c75c338264ab9290e
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-08-26 21:38:19 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState } from "react";
+import './App.css'
 import useWebAnimations from "@wellyshen/use-web-animations";
 
-const App = () => {
-  const element = useRef(null);
-  const { ref, playState } = useWebAnimations({
-    keyframes: {
-      transform: ["translateX(500px)"], // Move by 500px
-      background: ["red", "blue", "green"], // Go through three colors
-    },
+function App() {
+  const [Count, Setcount] = useState(1);
+  // const element = useRef(null);
+  const { ref, playState, getAnimation } = useWebAnimations({
+    keyframes: [
+      { transform: "translate(0,0)" },
+      { transform: "translate(800px,0)" }
+    ],
     timing: {
-      delay: 500, // Start with a 500ms delay
-      duration: 1000, // Run for 1000ms
-      iterations: 2, // Repeat once
+      // delay: 400, // Start with a 500ms delay
+      duration: 2500, // Run for 1000ms
+      iterations: Infinity, // Repeat once
       direction: "alternate", // Run the animation forwards and then backwards
       easing: "ease-in-out", // Use a fancy timing function
     },
-    // onReady: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
-    // },
-    // onUpdate: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation enters the running state or changes state
-    // },
-    // onFinish: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-    // },
-    // More useful options...
+    onReady: ({ playState, animate, animation }) => {
+      Setcount(Count++);
+    },
+    onUpdate: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the running state or changes state
+    },
+    onFinish: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    },
   });
 
   return (
     <div>
       <div className="target" ref={ref}>
-        Hello World!
       </div>
+              current animation state: {playState}
+      <button onClick={() => getAnimation().pause()}>Pause</button>
+      <button onClick={() => getAnimation().play()}>Play</button>
     </div>
   );
 };
